b0224dfc4a08303bcd669336418e7142c8c2d7ef
daus1983 <92921705+daus1983@users.noreply.github.com>
2021-10-21 17:39:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php session_start(); ?>
+<html>
+<head>
+	<title>Homepage</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<div id="header">
+		Welcome Tes Univesal Futures!
+	</div>
+	<?php
+	if(isset($_SESSION['valid'])) {			
+		include("connection.php");					
+		$result = mysqli_query($mysqli, "SELECT * FROM login");
+	?>
+				
+		Welcome <?php echo $_SESSION['name'] ?> ! <a href='logout.php'>Logout</a><br/>
+		<br/>
+		<a href='view.php'>View and Add Products</a>
+		<br/><br/>
+	<?php	
+	} else {
+		echo "You must be logged in to view this page.<br/><br/>";
+		echo "<a href='login.php'>Login</a> | <a href='register.php'>Register</a>";
+	}
+	?>
+	<div id="footer">
+		Created by <a href="https://www.semuabis.com/aqiilah-tour-travel-0815-8683-0621" title="Mukesh Chapagain">Mochamad Firdaus</a>
+	</div>
+</body>
+</html>
